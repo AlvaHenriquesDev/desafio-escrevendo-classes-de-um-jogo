1f2a0c0a013d8fb246b18d1b55f96e7359a0610b
Alvaro Henriques <alvahenriquesd@gmail.com>
2025-12-03 03:20:55 -0300

    Projeto principal do desafio concluido

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-const tipo = 'Guerreiro'
-const golpe = 'Espada'
+class Heroi {
+    constructor(role, weapon) {
+        this.role = role,
+        this.weapon = weapon
+    }
 
-function atacar() {
-    const messageElement = document.querySelector('#card__action_result');
-     messageElement.innerText = `O ${tipo} atacou usando ${golpe}`;
-     return messageElement
+    atacar() {
+        const messageElement = document.querySelector('#card__action_result');
+        messageElement.innerText = `O ${this.role} atacou usando ${this.weapon}`;
+        return messageElement
+    }
 }
+
+const class1 = new Heroi('Guerreiro', 'Excalibur')
+const class2 = new Heroi('Mago', 'Bola de Fogo')
+const class3 = new Heroi('Ladino', 'Adaga Envenenada')
+const class4 = new Heroi('Paladino','Luz Divina')
+const class5 = new Heroi('Arqueiro', 'Flecha de Prata')
